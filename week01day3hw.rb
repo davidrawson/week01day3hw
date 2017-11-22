807135533601a421ccbb54323321bafe40107f46
David Rawson <dm.rawson@gmail.com>
2017-11-22 21:14:32 +0000

    Section B completed

--- week01day3hw.rb
+++ week01day3hw.rb
@@ -6,31 +6,31 @@
 stops = [ "Croy", "Cumbernauld", "Falkirk High", "Linlithgow", "Livingston", "Haymarket" ]
 
 
-# 1. Add `"Edinburgh Waverley"` to the end of the array
-stops.push("Edinburgh Waverley")
-# 2. Add `"Glasgow Queen St"` to the start of the array
-stops.unshift("Glasgow Queen St")
-# 3. Add `"Polmont"` at the appropriate point (between `"Falkirk High"` and `"Linlithgow"`)
-stops.insert(4, "Polmont")
-# 4. Work out the index position of `"Linlithgow"`
-stops.index("Linlithgow")
-# 5. Remove `"Livingston"` from the array using its name
-stops.delete("Livingston")
-# 6. Delete `"Cumbernauld"` from the array by index
-stops.delete_at(2)
-# 7. How many stops there are in the array?
-stops.count()
-# 8. How many ways can we return `"Falkirk High"` from the array?
-stops[2]
-stops[-5]
-stops.fetch(2)
-stops.fetch(-5)
-# 9. Reverse the positions of the stops in the array
-stops.reverse!()
-# 10. Print out all the stops using a for loop
-for stop in stops
-  p stop
-end
+# # 1. Add `"Edinburgh Waverley"` to the end of the array
+# p stops.push("Edinburgh Waverley")
+# # 2. Add `"Glasgow Queen St"` to the start of the array
+# p stops.unshift("Glasgow Queen St")
+# # 3. Add `"Polmont"` at the appropriate point (between `"Falkirk High"` and `"Linlithgow"`)
+# p stops.insert(4, "Polmont")
+# # 4. Work out the index position of `"Linlithgow"`
+# p stops.index("Linlithgow")
+# # 5. Remove `"Livingston"` from the array using its name
+# p stops.delete("Livingston")
+# # 6. Delete `"Cumbernauld"` from the array by index
+# p stops.delete_at(2)
+# # 7. How many stops there are in the array?
+# p stops.count()
+# # 8. How many ways can we return `"Falkirk High"` from the array?
+# p stops[2]
+# p stops[-5]
+# p stops.fetch(2)
+# p stops.fetch(-5)
+# # 9. Reverse the positions of the stops in the array
+# p stops.reverse!()
+# # 10. Print out all the stops using a for loop
+# for stop in stops
+#   p stop
+# end
 
 ## B. Given the following data structure:
 
@@ -64,18 +64,35 @@ end
         "colin" => :snake
       }
     },
+    "David" => {
+      :twitter => "not_telling_until_ive_deleted-the-swearing",
+      :favourite_numbers => [8, 23, 64],
+      :home_town => "Edinburgh",
+      :pets => {
+        "maggie" => :cat,
+        "dora" => :cat
+      }
+    }
   }
 
 # 1. Get Jonathan's Twitter handle (i.e. the string `"jonnyt"`)
+p users["Jonathan"][:twitter]
 # 2. Get Erik's hometown
+p users["Erik"][:home_town]
 # 3. Get the array of Erik's favourite numbers
+p users["Erik"][:favourite_numbers]
 # 4. Get the type of Avril's pet Colin
+p users["Avril"][:pets]["colin"]
 # 5. Get the smallest of Erik's favourite numbers
+p users["Erik"][:favourite_numbers].min
 # 6. Add the number `7` to Erik's favourite numbers
+p users["Erik"][:favourite_numbers].push(7)
 # 7. Change Erik's hometown to Edinburgh
+p users["Erik"][:home_town] = "Edinburgh"
 # 8. Add a pet dog to Erik called "Fluffy"
+p users["Erik"][:pets][:dog] = "Fluffy"
 # 9. Add yourself to the users hash
-
+p users["David"][:pets]["dora"]
 # C. Given the following data structure:
 
 
